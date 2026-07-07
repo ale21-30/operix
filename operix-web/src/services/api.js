c9ef907004f8559dac3523aeaf428d7fc7a69fac
ale21-30 <ale512127@gmail.com>
2026-07-06 20:01:35 -0500

    admin fix

--- operix-web/src/services/api.js
+++ operix-web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://operix-production-052c.up.railway.app/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
